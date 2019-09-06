50fc094ba0be64644f4091fa3cb58c88a8b97851
leotolkein <lyuzhang@umich.edu>
2019-09-05 20:03:13 -0400

    initialization

---
+++ TwoSum/TwoSum.cpp
@@ -0,0 +1,11 @@
+#include "two_sum.hpp"
+
+std::vector<int> Rearrange(std::vector<int>& nums)
+{ // rearrange the list in order from smallest to largest
+    
+}
+
+std::vector<int> TwoSum(std::vector<int>& nums, int target)
+{ // 
+    
+}
---
+++ TwoSum/two_sum.hpp
@@ -0,0 +1,14 @@
+#ifndef __TWOSUM_H__
+#define __TWOSUM_H__
+
+#include <vector>
+
+class Solution {
+public:
+    // The expected answer
+    std::vector<int> TwoSum(std::vector<int>& nums, int target);
+    // rearrange the list in order from smallest to largest
+    std::vector<int> Rearrange(std::vector<int>& nums);
+};
+
+#endif // !__TWOSUM_H__
